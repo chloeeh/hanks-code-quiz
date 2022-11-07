ac6158561a6bcd3e5ecea1610eccd46647faab86
Todey <chloe.e.hanks@gmail.com>
2022-11-07 14:42:34 -0600

    added comments to HTML and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,9 +27,8 @@ var clearScoresBtn = document.querySelector("#clear-scores-btn");
 
 
 // Global Variables -------------------------------
-// Array of Objects -------------------------------
-// Create an array of objects to hold the question, user options, and answer
 
+// Create an array of objects to hold the question, user options, and answer
 let quizArray = [
     {
         question: "Commonly used data types DO NOT include: ",
@@ -53,6 +52,7 @@ let quizArray = [
     },
 ];
 
+// GLOBALS ------------------------------
 var lastQuestionIndex = quizArray.length-1;
 var currentQuestionIndex = 0;
 var points = 0;
@@ -67,22 +67,22 @@ var timerInterval = setInterval(timer, 1000);
 
 // FUNCTIONS ----------------------------
 
+// Initialized variables, sets the game to the beginning
 function init() {
-    // time span = timeRemaining;
-    // time = 0;
-    timeRemaining = 60;
-    penaltySeconds = 15;
-    clearInterval(timerInterval);
-    timerDisplay.textContent = timeRemaining;
-    feedbackMessage.innerHTML = "";
-    points = 0;
-    currentQuestionIndex = 0;
+    timeRemaining = 60;               // start time
+    penaltySeconds = 15;              // if answer is wrong, decrease remaining time by the penalty
+    clearInterval(timerInterval);    
+    timerDisplay.textContent = timeRemaining;   // display the start time on the screen
+    feedbackMessage.innerHTML = "";             // show an empty feedback message
+    points = 0;                       // set points back to 0
+    currentQuestionIndex = 0;         // set index back to 0 to start at first question
     score.textContent = points;
-    var storedHighScores = JSON.parse(localStorage.getItem("arrayInitials"));
+    var storedHighScores = JSON.parse(localStorage.getItem("arrayInitials"));   // manage local storage on init
     if (storedHighScores !== null) {
         arrayInitials = storedHighScores;
     }
     renderHighScores();
+    // set blocks of HTML to show the start screen and button only on init
     start.style.display = "block";
     startBtn.style.display = "block";
     quiz.style.display = "none";
@@ -90,16 +90,21 @@ function init() {
     highScore.style.display = "none";
 }
 
+// Create the timer function
 function timer() {
         timeRemaining--;
         timerDisplay.textContent = timeRemaining;
 
+        // when timer runs out, clear the timer and go to the quizComplete function
         if (timeRemaining === 0) {
             clearInterval(timerInterval);
             quizComplete();
         }
 }
 
+// When user clicks start button, go to quiz content
+// start timer
+// move to displayQuestion()
 function startQuiz(event) {
     event.preventDefault();
     timerInterval = setInterval(timer, 1000);
@@ -110,11 +115,8 @@ function startQuiz(event) {
 
 }
 
+// Show the question stored at the currentQuestionIndex inside the quizArray
 function displayQuestion() {
-
-    if (currentQuestionIndex > 0) {
-        document.getElementById(userChoiceId).style.backgroundColor = "#6c757d";
-    }
     var myQuestion = quizArray[currentQuestionIndex];
 
     // populate the question based on the object at the index
@@ -124,9 +126,13 @@ function displayQuestion() {
     for (var i = 0; i < choiceArray.length; i++) {
         choiceArray[i].textContent = myQuestion.choices[i];
     }
-    // currentQuestionIndex++;
 }
 
+// Compare the button ID that the user selects to the answer in the quizArray
+// display feedback message to user on their answer selection
+// increment the currentQuestionIndex to go to the next question
+// if the index is higher than the last question index in the quizArray, go to quizComplete
+// else go to the next questions
 function checkAnswer() {
     var correctAns = quizArray[currentQuestionIndex].answer;
     if (userChoiceId === correctAns) {
@@ -146,6 +152,8 @@ function checkAnswer() {
     }
 }
 
+// if the user selects correct answer, increment points
+// display "CORRECT" to the user for 1 sec
 function answerIsCorrect() {
     points++;
 
@@ -155,6 +163,8 @@ function answerIsCorrect() {
     }, 1000)
 }
 
+// if the user selects wrong answer, take off time from the time remaining
+// display "WRONG" to the user for 1 sec
 function answerIsWrong() {
     timeRemaining -= penaltySeconds;
     
@@ -164,6 +174,8 @@ function answerIsWrong() {
     }, 1000)
 }
 
+// Go to All Done! page to submit user initials
+// clear the timer
 function quizComplete() {
     quiz.style.display = "none";
     allDone.style.display = "block";
@@ -174,13 +186,13 @@ function quizComplete() {
     timerDisplay.textContent = timeRemaining;
 }
 
+// Go to High Score page and show the scores of the user with their submitted initials
 function renderHighScores() {
-    // event.preventDefault();
     allDone.style.display = "none";
     highScore.style.display = "block";
     scoreList.innerHTML = "";
 
-     // Render a new li for each todo
+     // Render a new li element for each user initials which includes their score
     for (var i = 0; i < arrayInitials.length; i++) {
         var updateInitials = arrayInitials[i];
         var updateScores = arrayScores[i];
@@ -188,21 +200,18 @@ function renderHighScores() {
         var li = document.createElement("li");
         li.textContent = updateInitials + " ---- " + updateScores;
         li.setAttribute("data-index", i);
-
-        // var button = document.createElement("button");
-        // button.textContent = "Complete ✔️";
-
-        // li.appendChild(button);
         scoreList.appendChild(li);
     }
 }
 
+// store the user text and score in locals storage
 function storeHighScores() {
     // Stringify and set key in localStorage to todos array
     localStorage.setItem("arrayInitials", JSON.stringify(arrayInitials));
     localStorage.setItem("arrayScores", JSON.stringify(arrayScores));
   }
 
+// clear the high scores from local storage
   function clearHighScores() {
     arrayInitials.splice(0, arrayInitials.length);
     arrayScores.splice(0, arrayScores.length);
@@ -210,23 +219,28 @@ function storeHighScores() {
     renderHighScores();
   }
 
+// Call init
+init();
 
 // EVENT HANDLERS -----------------------
-init();
 // When user clicks "start" button, begin the startQuiz function
 startBtn.addEventListener("click", startQuiz);
-// choiceBtn.addEventListener("click", checkAnswer);
+
+// When user selects an answer grab the button's id and move to checkAnswer()
 choiceBtn.forEach(userChoice => {
     userChoice.addEventListener('click', function handleEvent(event) {
         userChoiceId = userChoice.id;
         checkAnswer();
     })
 })
+
+// When user submits their initials, push their initials and their score to arrays,
+// store the information
+// render the information
 submitBtn.addEventListener("click", function(event) {
     event.preventDefault();
 
     var initialsText = userInitials.value.trim();
-
     if (initialsText === "") {
         return;
     }
@@ -237,13 +251,19 @@ submitBtn.addEventListener("click", function(event) {
 
     storeHighScores();
     renderHighScores();
-
 });
 
+// When user cliks Restart, go back to the initial page
 restartBtn.addEventListener("click", init);
+// When user cliks the Clear Scores button, go to clearHighScores function
 clearScoresBtn.addEventListener("click", clearHighScores);
+// at any time, when user wants to view the high scores, 
+// render the scores
+// show the high score page
+// clear the timer
 viewScoreBtn.addEventListener("click", function(event) {
     renderHighScores();
+
     start.style.display = "none";
     startBtn.style.display = "none";
     quiz.style.display = "none";
@@ -253,4 +273,4 @@ viewScoreBtn.addEventListener("click", function(event) {
     clearInterval(timerInterval);
     timeRemaining = 0;
     timerDisplay.textContent = timeRemaining;
-})
+});
